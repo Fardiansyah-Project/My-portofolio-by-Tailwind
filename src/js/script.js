1d6999acc9d9c386aa316dcdda960b0798330b87
Fardiansyah-Project <fardiansyahadhiguna@gmail.com>
2024-11-07 08:30:04 +0800

    create featur darkmode

--- src/js/script.js
+++ src/js/script.js
@@ -11,10 +11,46 @@ hamburger.addEventListener("click", function () {
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const backButon= document.querySelector("#back_button");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("nav-fixed");
+    backButon.classList.remove("hidden");
+    backButon.classList.add("flex");
   } else {
     header.classList.remove("nav-fixed");
+    backButon.classList.remove("flex");
+    backButon.classList.add("hidden");
   }
 };
+
+// Click out area navbar
+// window.addEventListener('click', function (e) {
+//   if(e.target != hamburger && e.target != navMenu) {
+//     hamburger.classList.remove("hamburger-active");
+//     navMenu.classList.add("hidden");
+//   }
+// });
+
+// Darkmode function
+const darkButton = document.querySelector("#dark_togle");
+const html = document.querySelector("html");
+
+darkButton.addEventListener("click", function() {
+  if (darkButton.checked){
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  }
+  else{
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+if (localStorage.theme === "dark" || (!('theme' in localStorage) && window.matchMedia(
+  "(prefers-color-scheme = dark)").matches)) {
+    darkButton.checked = true;
+  }
+  else{
+    darkButton.checked = false;
+  }
