288111cb6aaea6f947d3285bd5b351257f016038
Fardiansyah-Project <fardiansyahadhiguna@gmail.com>
2024-11-13 15:21:01 +0800

    Menambahkan animasi

--- src/js/script.js
+++ src/js/script.js
@@ -12,11 +12,13 @@ window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
   const backButon= document.querySelector("#back_button");
+  const id= document.querySelector("#home-section");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("nav-fixed");
     backButon.classList.remove("hidden");
     backButon.classList.add("flex");
+    backButon.classList.add("opacity");
   } else {
     header.classList.remove("nav-fixed");
     backButon.classList.remove("flex");
@@ -26,10 +28,10 @@ window.onscroll = function () {
 
 // Click out area navbar
 window.addEventListener('click', function (e) {
-   if(e.target != hamburger && e.target != navMenu) {
-   hamburger.classList.remove("hamburger-active");
-   navMenu.classList.add("hidden");
-   }
+  if(e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
 });
 
 // Darkmode function
@@ -54,3 +56,40 @@ if (localStorage.theme === "dark" || (!('theme' in localStorage) && window.match
   else{
     darkButton.checked = false;
   }
+
+  // Animation scroll
+  document.addEventListener('scroll', function() {
+    const animation1 = document.getElementById('animation1');
+    const sectionPosition = animation1.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (sectionPosition < screenHeight && sectionPosition > 1) {
+      animation1.classList.add('opacity-100');
+      animation1.classList.remove('opacity-0');
+      animation1.classList.add('scale-100');
+      animation1.classList.remove('scale-0');
+    } else {
+      animation1.classList.add('opacity-0');
+      animation1.classList.remove('opacity-100');
+      animation1.classList.add('scale-0');
+      animation1.classList.remove('scale-100');
+    }
+  });
+
+  document.addEventListener('scroll', function() {
+    const animation2 = document.getElementById('animation2');
+    const sectionPosition = animation1.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (sectionPosition < screenHeight && sectionPosition > 1) {
+      animation2.classList.add('opacity-100');
+      animation2.classList.remove('opacity-0');
+      animation2.classList.add('scale-100');
+      animation2.classList.remove('scale-0');
+    } else {
+      animation2.classList.add('opacity-0');
+      animation2.classList.remove('opacity-100');
+      animation2.classList.add('scale-0');
+      animation2.classList.remove('scale-100');
+    }
+  });
